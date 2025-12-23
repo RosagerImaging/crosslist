f6266c19543ca3ca3e7c3cada6b1e81deac0f9cf
Rosagerimaging <Rosagerimaging@gmail.com>
2025-12-22 19:24:51 -0800

    fix: middleware

--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -6,6 +6,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  defaultIntegrations: false,
 
   // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing
   // We recommend adjusting this value in production
